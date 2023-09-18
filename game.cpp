79a7fd1f2bbb51684cef841f29b44bdb8ef15bd2
jessshu <jessica.minhu@gmail.com>
2023-09-18 14:55:52 +0930

    mac fixes n beta squared testing

--- game.cpp
+++ game.cpp
@@ -16,7 +16,7 @@ void game::run() {
   // object creations and generations here
   platform p1(100.0f, 100.0f, sf::Vector2f(50.0f, 50.0f));
   Entity e1(sf::Vector2f(100.0f, 100.0f), 100.0f, 250.0f);
-  Player pl1(sf::Vector2f(100.0f, 100.0f), 10.0f, 10.0f);
+  Player pl1(sf::Vector2f(10.0f, 10.0f), 10.0f, 10.0f);
 
   while (Window->isOpen()) {
     sf::Event event;
