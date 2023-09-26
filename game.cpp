c568cfdbf11caa05e208f8eb2ecf29c71b2cd34e
spb-59 <shreeshailpbhatta@gmail.com>
2023-09-26 12:29:57 +0930

    added movemnet

--- game.cpp
+++ game.cpp
@@ -12,7 +12,7 @@ game::game() {
   Window = new sf::RenderWindow(sf::VideoMode(800, 800),
                                 "Game");  // making game window
   background.loadFromFile("tex/background.png");
- // sf::Sprite bg(background);
+  // sf::Sprite bg(background);
 
   if (texture.loadFromFile("tex/yipppe.png")) {
     std::cout << "Failed to load";
@@ -24,7 +24,9 @@ void game::run() {
   platform p1(100.0f, 100.0f, sf::Vector2f(50.0f, 50.0f));
   Entity e1(sf::Vector2f(1.0f, 1.0f), 100.0f, 250.0f, &texture);
   Player pl1(sf::Vector2f(1.0f, 1.0f), 10.0f, 10.0f, &texture);
-  Entity bg(sf::Vector2f(1.0f, 1.0f), 0.0f,0.0f, &background);
+  Entity bg(sf::Vector2f(1.0f, 1.0f), 0.0f, 0.0f, &background);
+
+  sf::View view(sf::FloatRect(0, 0, Window->getSize().x, Window->getSize().y));
 
   while (Window->isOpen()) {
     sf::Event event;
@@ -37,15 +39,22 @@ void game::run() {
     pl1.movement(event);
 
     // all rendering logic here
+    sf::Vector2f cameraPosition = view.getCenter();
+    cameraPosition.x += 0.01f;
 
-    // test render background 
+    // Update the view's center to follow the player
+    view.setCenter(cameraPosition);
 
-        bg.render(Window);
+    // Apply the view to the window
+    Window->setView(view);
+
+    // test render background
+
+    bg.render(Window);
 
     p1.render(Window);
     e1.render(Window);
     pl1.render(Window);
-
 
     Window->display();
   }
