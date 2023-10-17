12e228d01b35acda1f3bd3dbbb33cad94685fbc0
khanhgn <127292124+khanhgn@users.noreply.github.com>
2023-10-17 22:23:29 +1030

    final

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -19,6 +19,8 @@ class Entity : public Object {
   void movement(sf::Event event);
   void setSpeed(sf::Vector2f speed);
   sf::Vector2f getSpeed();
+  void set_landed(bool landed);
+
   std::string get_type();
 };
 
