2d7efd37d32727d427e0b776e3d31a41e9a9852c
spb-59 <shreeshailpbhatta@gmail.com>
2023-10-17 17:28:16 +1030

    trying movment

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -13,53 +13,63 @@ Entity::Entity(float x_cord, float y_cord, sf::Vector2f size)
 void Entity::movement(sf::Event event) {
   // std::cout << x_cord_ << " ";
   if (canMove) {
-    switch (event.type) {
-      case sf::Event::KeyPressed:
-        // Handle specific key events for movement
-        switch (event.key.code) {
-          case sf::Keyboard::Up:
-            // if (can_jump) {
-            // can_jump = false;
-            speed.y = -0.30f;
-            break;
-            // } else {
-            //   break;
-            // }
-          // case sf::Keyboard::Down:
-          //   speed.y = 0.50f;
-          //  break;
-          case sf::Keyboard::Left:
-            speed.x = -0.70f;
-            break;
-          case sf::Keyboard::Right:
-            speed.x = 0.70f;
-            break;
-          default:
-            break;
-        }
-        break;
-      case sf::Event::KeyReleased:
-        // Stop movement when the key is released
+    // switch (event.type) {
+    //   case sf::Event::KeyPressed:
+    //     // Handle specific key events for movement
+    //     switch (event.key.code) {
+    //       case sf::Keyboard::Up:
+    //         // if (can_jump) {
+    //         // can_jump = false;
+    //         speed.y = -0.30f;
+    //         break;
+    //         // } else {
+    //         //   break;
+    //         // }
+    //       // case sf::Keyboard::Down:
+    //       //   speed.y = 0.50f;
+    //       //  break;
+    //       case sf::Keyboard::Left:
+    //         speed.x = -0.70f;
+    //         break;
+    //       case sf::Keyboard::Right:
+    //         speed.x = 0.70f;
+    //         break;
+    //       default:
+    //         break;
+    //     }
+    //     break;
+    //   case sf::Event::KeyReleased:
+    //     // Stop movement when the key is released
 
-        switch (event.key.code) {
-          case sf::Keyboard::Up:
-            can_jump = true;
-            speed.y -= 0.0f;
-            break;
-          case sf::Keyboard::Left:
-            speed.x = 0;
-            break;
-          case sf::Keyboard::Right:
-            speed.x = 0;
-            break;
-          default:
-            break;
-        }
-        break;
-      default:
-        break;
+    //     switch (event.key.code) {
+    //       case sf::Keyboard::Up:
+    //         can_jump = true;
+    //         speed.y -= 0.0f;
+    //         break;
+    //       case sf::Keyboard::Left:
+    //         speed.x = 0;
+    //         break;
+    //       case sf::Keyboard::Right:
+    //         speed.x = 0;
+    //         break;
+    //       default:
+    //         break;
+    //     }
+    //     break;
+    //   default:
+    //     break;
+    // }
+    if (event.type == sf::Event::KeyPressed) {
+      if (event.key.code == sf::Keyboard::Up) {
+        speed.y = -1.0f;
+      }
     }
 
+    // } else if (can_jump && speed.y > 10.0f) {
+    //   speed.y -= -0.2f;
+    // }
+    std::cout << speed.y;
+    speed.x = 0.3f;
     obj.move(speed);
   }
 }
